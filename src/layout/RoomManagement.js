2efb7614f3e52109cf0ab90fd004ec6f82fe6ff0
LemonDev <cnnhok6262@gmail.com>
2023-10-22 00:04:27 +0700

    ok

---
+++ src/layout/RoomManagement.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+const RoomManagement = () => {
+  return (
+    <div>
+      <Header />
+      RoomManagement
+    </div>
+  );
+};
+
+export default RoomManagement;
